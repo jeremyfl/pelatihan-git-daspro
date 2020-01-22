560461ff6438eb50b65cefb352b549d5b280826b
Jeremy Lombogia <jeremylombogia7@gmail.com>
2020-01-22 18:58:29 +0700

    Initial commit

---
+++ challange.js
@@ -0,0 +1,17 @@
+class Hmn {
+  constructor() {
+    this.name = 'John'
+    this.age = 20
+    this.isMale = 'true'
+  }
+
+  countnumber(fn, sn) {
+    return fn + sn
+  }
+
+  eat() {
+    console.log('People Speaking')
+
+    return 'People Eating'
+  }
+}
---
+++ manusia.js
@@ -0,0 +1,19 @@
+class Human {
+  constructor() {
+    this.name = 'John'
+    this.age = 20
+    this.isMale = true
+  }
+
+  speak() {
+    return 'People Speaking'
+  }
+
+  count(firstNumber, secondNumber) {
+    return firstNumber + secondNumber
+  }
+
+  eat() {
+    return 'People Eating'
+  }
+}
